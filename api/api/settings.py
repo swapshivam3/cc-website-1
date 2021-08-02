efad0ca76a0919e2d61291f57785caaca6fd292b
swapshivam3 <swapshivam3@gmail.com>
2021-08-02 16:43:34 +0530

    add VisitorRegister View

--- api/api/settings.py
+++ api/api/settings.py
@@ -38,8 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    
+
     # Apps (Modules)
+    'rest_framework',
     'blog.apps.BlogConfig',
     'exam.apps.ExamConfig',
     'main.apps.MainConfig',
