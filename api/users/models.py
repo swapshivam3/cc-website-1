e127dbc6a7983042de779f1c7847b7a1eadb641e
swapshivam3 <swapshivam3@gmail.com>
2021-08-09 13:54:36 +0530

    add passsword reset and login/logout

--- api/users/models.py
+++ api/users/models.py
@@ -19,7 +19,9 @@ class CustomUser(AbstractUser):
     email = models.EmailField(verbose_name = "Email", max_length=254, unique = True)
     name = models.CharField(verbose_name = "Name", max_length=50)
     username = models.CharField(max_length=255, blank = True, null = True, default = "user")    
-    
+    is_visitor = models.BooleanField(default=False)
+    is_candidate = models.BooleanField(default=False)
+    is_member = models.BooleanField(default=False)
     REQUIRED_FIELDS = ['name']
     USERNAME_FIELD = 'email'
 
@@ -27,19 +29,26 @@ class CustomUser(AbstractUser):
         return self.email
     
     def save(self, *args, **kwargs):
+        self.username = self.email
         super().save(*args, **kwargs)
         
-        self.username = self.email
+    def check_password(self, raw_password):
+        if self.password == raw_password:
+            return True
+        else:
+            return False
+        
 
     objects = UserManager()
 
 
 # 1. Visitor    --- Aditya
 
-class Visitor(CustomUser):
+class Visitor(models.Model):
     '''
     Visitor class inherits CustomUser and has additional fields of phone, interests and city
     '''
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE, primary_key=True,related_name="visitor")
     phone = models.PhoneNumberField(unique=True)
     interests = models.TextField(blank=True, max_length=100) #optional field
     city = models.CharField(blank=True,max_length=20) #optional field
@@ -62,7 +71,7 @@ class Member(models.Model):
         ('ui', 'UI/UX'),
     )
 
-    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name="member")
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE, primary_key=True,related_name="member")
     bits_id = models.CharField(max_length=50, blank=False, null=False, verbose_name="BITS ID")
     bits_email = models.EmailField(max_length=100, verbose_name="BITS Email", blank=False, null=False)
     department = models.CharField(choices=departments, blank=False, null=False,max_length=2)
@@ -86,17 +95,17 @@ gender_choices = (
 	('O','Others')
 )
 
-class Candidate (CustomUser) :
+class Candidate (models.Model) :
 	
     '''
     A user who registers for recrutiment is a candidate. 
     Candidate class inerits CustomUser and had additionally gender , githubid , first to fifth priority of any candidate
     field_validate function checks if any choice is repeated in the priority of candidate
     '''
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE, primary_key=True,related_name="candidate")
     gender= models.CharField(max_length=1,choices=gender_choices)
     bits_id = models.CharField(verbose_name="BITS ID",max_length=13,unique=True,blank=False)
     githubid=models.CharField(verbose_name="Github ID",max_length=30,unique=True,blank=True)
-
     pr1 = models.CharField(verbose_name="First Priority",max_length=2,choices=departments,default=None)
     pr2 = models.CharField(verbose_name="Second Priority",max_length=2,choices=departments,default=None)
     pr3 = models.CharField(verbose_name="Third Priority",max_length=2,choices=departments,default=None)
--- api/users/serializers.py
+++ api/users/serializers.py
@@ -1,9 +1,14 @@
 from rest_framework import serializers
-from .models import Visitor
+from .models import CustomUser, Visitor
 
+class CustomUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = '__all__'
+        extra_kwargs = {'password': {'write_only': True}}
 
 class VisitorSerializer(serializers.ModelSerializer):
+    user=CustomUserSerializer(required=False)
     class Meta:
         model = Visitor
         fields = '__all__'  # change later
-        extra_kwargs = {'password': {'write_only': True}}
