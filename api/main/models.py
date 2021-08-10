52b9174dbd24e69adc650530f49816306ddb3a60
Dhruv-Singh1 <dschoudhery1911@gmail.com>
2021-08-11 00:20:54 +0530

    Feedback model and views added

--- api/main/models.py
+++ api/main/models.py
@@ -1,6 +1,15 @@
+from api.users.models import Visitor
+from api import users
 from django.db import models
+from users.models import Visitor
 
 # Feedback ----  Dhruv
+
+class Feedback(models.Model):
+    user = models.ForeignKey(Visitor)
+    date = models.DateField(auto_now_add=True)
+    review = models.TextField(help_text="Write your valuable feedback")
+
 
 # Departments ---- Aditya
 
