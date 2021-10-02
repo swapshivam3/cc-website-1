5b1e77173b4382c3dc8499534599754d51558fe9
satviksinha <f20200993@pilani.bits-pilani.ac.in>
2021-10-02 14:51:19 +0530

    portal modified

--- frontend/src/Components/Exam/Exam.js
+++ frontend/src/Components/Exam/Exam.js
@@ -1,32 +1,17 @@
 import React, { useState } from "react";
-import Profile from "../Profile/Profile";
 import { useSelector } from "react-redux";
 
 const Exam = () => {
-  const [show, setShow] = useState(false);
   const dataArray = useSelector((state) => state.questions);
   if (dataArray) {
     console.log(dataArray.data);
   }
-
-  const showdataArray = () => {
-    setShow(!show);
-  };
   return (
     <div className="relative z-10 text-white text-center text-2xl font-bold">
       <h1>EXAM PORTAL</h1>
-      <Profile />
-      <button
-        className="bg-red-700 relative top-12 rounded-md p-3"
-        onClick={showdataArray}
-      >
-        Start test
-      </button>
-      {show
-        ? dataArray === undefined
-          ? dataArray.data.map((question) => <div>{question.qtxt}</div>)
-          : "BACKEND ERROR"
-        : null}
+      {dataArray === undefined
+        ? dataArray.data.map((question) => <div>{question.qtxt}</div>)
+        : "BACKEND ERROR"}
     </div>
   );
 };
--- frontend/src/Components/Profile/Profile.js
+++ frontend/src/Components/Profile/Profile.js
@@ -1,14 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
+import Exam from "../Exam/Exam";
 
 const Profile = () => {
+  const [show, setShow] = useState(false);
+
+  const showdataArray = () => {
+    setShow(!show);
+  };
   return (
-    <div className=" flex justify-center items-center">
-      <div className=" bg-red-900 h-64 w-56 mt-24 flex flex-col justify-around items-center">
-        <div className="bg-red-600 h-24 w-24 rounded-full flex items-center justify-center">
-          <h3 className=" text-6xl">S</h3>
+    <div className="flex flex-col items-center">
+      <div className=" flex justify-center items-center">
+        <div className=" bg-red-900 h-64 w-56 mt-24 flex flex-col justify-around items-center">
+          <div className="bg-red-600 h-24 w-24 rounded-full flex items-center justify-center">
+            <h3 className=" text-6xl">S</h3>
+          </div>
+          <h3>Satvik Sinha</h3>
         </div>
-        <h3>Satvik Sinha</h3>
       </div>
+      <button
+        className="bg-red-700 relative top-12 rounded-md p-3 mb-12"
+        onClick={showdataArray}
+      >
+        Start test
+      </button>
+      {show ? <Exam /> : null}
     </div>
   );
 };
