9f25467bf34f4ae68529b7900e8a4a79fce77dac
Sarthak Tayal <f20200987@pilani.bits-pilani.ac.in>
2021-11-15 19:34:03 +0530

    alumini page

---
+++ frontend/src/Containers/Alumini/Alumini.js
@@ -0,0 +1,22 @@
+import TeamMemberCard from "../../Components/TeamMemberCard/TeamMemberCard";
+import React from 'react'
+
+const Alumini = () => {
+  return (
+    <div className="App">
+      <div className="grid md:grid-cols-2 grid-cols-1">
+        <TeamMemberCard name="Satvik Sinha" department="Frontend" />
+        <TeamMemberCard name="Nachiket Kotalwar" department="Frontend" />
+        <TeamMemberCard name="Anant Jain" department="Frontend" />
+        <TeamMemberCard name="Sarthak Tayal" department="Frontend" />
+        <TeamMemberCard name="Harsh Neema" department="Frontend" />
+        <TeamMemberCard name="Sanyam Garg" />
+        <TeamMemberCard name="Dhruv Singh" />
+        <TeamMemberCard />
+        <TeamMemberCard name="Aditya SSV" />
+      </div>
+    </div>
+  );
+};
+
+export default Alumini;
